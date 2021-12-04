b1a36b9bd53a6bd85d703af99ea9c4214c43dbe1
Ghost56 <94029083+ArchGhost56@users.noreply.github.com>
2021-12-04 16:28:37 +0800

    Update simplescanner.py

--- simplescanner.py
+++ simplescanner.py
@@ -1,5 +1,3 @@
-import sys
-import os
 import socket
 import _thread
 import time
@@ -27,7 +25,6 @@ class Core(object):
             print ("Invalid URL or IP")
             exit(0)
         Core.ipurl=self.ipurl
-        os.system("figlet Scanner")
         print (60*"-")
         print (22*" ",bcolors.FAIL,"Ghostscanner",bcolors.ENDC)
         print (60*"-")
